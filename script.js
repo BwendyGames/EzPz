c5f331333e66ca80f83092aec380da771ff5fdab
HollowNexus <110873294+HollowNexus@users.noreply.github.com>
2022-09-01 23:01:41 -0500

    init update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const boldBtn = document.querySelector('#bold-btn')
 const underlineBtn = document.querySelector('#underline-btn')
 const italicBtn = document.querySelector('#italic-btn')
+const indentButton = document.querySelector('#indent-btn')
+const undoButton = document.querySelector('#undo-btn')
+const redoButton = document.querySelector('#redo-btn')
+const hButton = document.querySelector('#h-btn')
 
 const txtBtn = document.querySelector('#txt-btn')
 const htmlBtn = document.querySelector('#html-btn')
@@ -8,6 +12,20 @@ const drkBtn = document.querySelector('#drk-btn')
 
 const content = document.querySelector("#content")
 const filename = document.querySelector("#filename-input")
+
+txtBtn.addEventListener('click', () => {
+    const a = document.createElement('a')
+    const blob = new Blob([content.innerText])
+    const dataUrl = URL.createObjectURL(blob)
+    a.href = dataUrl
+    a.download = filename.value + ".txt"
+    a.click() 
+})
+
+drkBtn.addEventListener('click', () => {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+})
 
 boldBtn.addEventListener('click', () => {
     document.execCommand('bold')
@@ -21,13 +39,20 @@ italicBtn.addEventListener('click', () => {
     document.execCommand('italic')
 })
 
-txtBtn.addEventListener('click', () => {
-    const a = document.createElement('a')
-    const blob = new Blob([content.innerText])
-    const dataUrl = URL.createObjectURL(blob)
-    a.href = dataUrl
-    a.download = filename.value + ".txt"
-    a.click() 
+indentButton.addEventListener('click', () => {
+    document.execCommand('indent')
+})
+
+hButton.addEventListener('click', () => {
+    document.execCommand('heading')
+})
+
+undoButton.addEventListener('click', () => {
+    document.execCommand('undo')
+})
+
+redoButton.addEventListener('click', () => {
+    document.execCommand('redo')
 })
 
 htmlBtn.addEventListener('click', () => {
@@ -37,18 +62,4 @@ htmlBtn.addEventListener('click', () => {
     a.href = dataUrl
     a.download = filename.value + ".html"
     a.click() 
-})
-
-drkBtn.addEventListener('click', () => {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-})
-
-
-const splash = document.querySelector('.splash');
-
-document.addEventListener('DOMContentLoaded', (e)=>{
-    setTimeout(()=>{
-        splash.classList.add('display-none');
-    }, 200);
 })
